e6f3e1ed6571c3482011c959803d0c2a60276d23
sdandge <saitej_dandge@intuit.com>
2022-07-20 14:57:53 +0530

    istio learning

--- v1/server.js
+++ v1/server.js
@@ -3,7 +3,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    res.status(200)
     res.send('Hello World! from v1')
+})
+
+app.get('/login', (req, res) => {
+    res.status(201)
+    res.send('Hello World login ! from v1')
+})
+
+app.get('/dashboard', (req, res) => {
+    res.status(202)
+    res.send('Hello World dashboard ! from v1')
 })
 
 app.listen(port, () => {
--- v2/server.js
+++ v2/server.js
@@ -1,11 +1,28 @@
+/**
+ * eval $(minikube docker-env)
+ * docker build -t user-image .
+ * minikube image build -t user-image:v1 .
+ * minikube image ls
+ */
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    res.status(200)
     res.send('Hello World! from v2')
 })
 
+app.get('/login', (req, res) => {
+    res.status(201)
+    res.send('Hello World login ! from v2')
+})
+
+app.get('/dashboard', (req, res) => {
+    res.status(202)
+    res.send('Hello World dashboard ! from v2')
+})
 app.listen(port, () => {
     console.log(`v2 listening on port ${port}`)
 })
